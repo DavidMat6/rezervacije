dad1f34d6230bd33c2f88b061f6f8dfec625ecd4
David <David@DESKTOP-4C45EQL>
2025-03-12 21:42:19 +0100

    Add project files.

---
+++ rezervacije/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace rezervacije
+{
+    public partial class Form1 : Form
+    {
+        private List<rezklasa> rezervacije = new List<rezklasa>();
+        private string filePath = "rezervacije.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadRezervacije();
+        }
+
+        private void LoadRezervacije()
+        {
+            if (File.Exists(filePath))
+            {
+                rezervacije = File.ReadAllLines(filePath)
+                    .Select(line => rezklasa.FromCsv(line))
+                    .ToList();
+            }
+            RefreshGrid();
+        }
+
+        private void SaveRezervacije()
+        {
+            File.WriteAllLines(filePath, rezervacije.Select(r => r.ToCsv()));
+        }
+
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int klijentID, uslugaID;
+
+          
+            if (!int.TryParse(textBox1.Text, out klijentID))
+            {
+                MessageBox.Show("Molimo unesite ispravan Klijent ID (broj).", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+           
+            if (!int.TryParse(textBox2.Text, out uslugaID))
+            {
+                MessageBox.Show("Molimo unesite ispravan Usluga ID (broj).", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+       
+            var novaRezervacija = new rezklasa(
+                rezervacije.Count + 1,
+                klijentID,
+                uslugaID,
+                dateTimePicker1.Value,
+                dateTimePicker2.Value.TimeOfDay);
+
+            rezervacije.Add(novaRezervacija);
+            SaveRezervacije();
+            RefreshGrid();
+        }
+        private void RefreshGrid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = rezervacije;
+        }
+
+       
+    }
+}
---
+++ rezervacije/rezklasa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rezervacije
+{
+    internal class rezklasa
+    {
+            
+        public int ID { get; set; }
+        public int KlijentID { get; set; }
+        public int UslugaID { get; set; }
+        public DateTime Datum { get; set; }
+        public TimeSpan Vrijeme { get; set; }
+
+        public rezklasa(int id, int klijentID, int uslugaID, DateTime datum, TimeSpan vrijeme)
+        {
+            ID = id;
+            KlijentID = klijentID;
+            UslugaID = uslugaID;
+            Datum = datum;
+            Vrijeme = vrijeme;
+        }
+
+        public string ToCsv()
+        {
+            return $"{ID},{KlijentID},{UslugaID},{Datum:yyyy-MM-dd},{Vrijeme}";
+        }
+
+        public static rezklasa FromCsv(string csvLine)
+        {
+            var values = csvLine.Split(',');
+            return new rezklasa(
+                int.Parse(values[0]),
+                int.Parse(values[1]),
+                int.Parse(values[2]),
+                DateTime.Parse(values[3]),
+                TimeSpan.Parse(values[4])
+            );
+        }
+    }
+}
